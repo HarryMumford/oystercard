55da7522198f379084137ebcb858a7e70fc97b91
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-18 15:18:43 +0000

    write tests for maximum limit on balance, code to pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
+  
+  MAXIMUM_LIMIT = 90
+  CANNOT_EXCEED_MAXIMUM_LIMIT = "operation denied: this transaction will exceed the £#{MAXIMUM_LIMIT} limit"
   
   def initialize
     @balance = 0
+    @limit = MAXIMUM_LIMIT
   end
 
   def top_up(amount)
+    raise CANNOT_EXCEED_MAXIMUM_LIMIT unless balance + amount <= MAXIMUM_LIMIT
+
+    @balance += amount
     "card was topped up by £#{amount}"
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,12 +5,27 @@ RSpec.describe Oystercard do
     it 'has a default balance of £0' do
       expect(subject.balance).to be 0
     end
+
+    it 'has a maximum limit' do
+      expect(subject.limit).to be Oystercard::MAXIMUM_LIMIT
+    end
   end
 
   describe '#top_up' do
     it "returns balance after topping up specified amount" do
       amount = 5
       expect(subject.top_up(amount)).to eq("card was topped up by £#{amount}")
+    end
+
+    it 'increases the card balance by the specified amount' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by 10
+    end
+
+    it 'should not allow a user to exceed the limit' do
+      almost_full_oyster = Oystercard.new
+      almost_full_oyster.top_up(Oystercard::MAXIMUM_LIMIT - 3)
+
+      expect { almost_full_oyster.top_up(5) }.to raise_error Oystercard::CANNOT_EXCEED_MAXIMUM_LIMIT
     end
   end
 end
