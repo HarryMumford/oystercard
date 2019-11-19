39b24bd9f3f69838bfbdc68b59ac2e3d381d479a
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-11-19 16:11:14 +0000

    ammend list_journeys method

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -1,7 +1,10 @@
 require './lib/oystercard.rb'
-card = Oystercard.new 
+require './lib/station.rb'
+card = Oystercard.new
+entry_station = Station.new("algate")
+exit_station = Station.new("bank")
+exit_station.name
 card.top_up(50)
-card.touch_in
-card.touch_out
+card.touch_in(entry_station)
+card.touch_out(exit_station)
 card.list_journeys
-=> [entry_station.name]
